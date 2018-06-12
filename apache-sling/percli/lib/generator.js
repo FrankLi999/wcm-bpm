0362fc95234ac15b0699d7bfe24e2490aa7a1cd4
Frank Li <frank.x.li@gmail.com>
2018-06-12 14:20:46 -0400

    Added apache-sling related projects

---
+++ apache-sling/percli/lib/generator.js
@@ -0,0 +1,198 @@
+const fs = require('fs')
+
+/** Class representing the sling model generator */
+class Generator {
+    /**
+        * Constructs the generator
+        * @param {string} source - the source
+        * @param {string} dest - the destination
+        * @param {string} info - the info
+    */
+    constructor(source, dest, info) {
+        this.src = source;
+        this.dst = dest;
+        this.data = info;
+    }
+}
+
+/**
+    * Substitutes all the values into the template
+    * @param {string} srcData - the source data string
+    * @param {string} generator - the generator object
+    * @return {string} the template
+*/
+function makeInitial(srcData, generator) {
+    var template = srcData.replace(/{{.*?}}/g, function(str, p1, offset, s) {
+        var key = str.trim().slice(2,str.trim().length-2).trim()
+        var ret = generator.data[key]
+        if(!ret) {
+            console.log('missing property:', key)
+            ret = '!!!'+key+'!!'
+        }
+        return ret
+    })
+    return template
+}
+
+/**
+    * Used in the gen function and applies all of the inject properties
+    * @param {string} properties - the properties from the model JSON
+    * @return {string} the inject string block
+*/
+function genNames(properties) {
+    inject = ''
+    for(var prop in properties) {
+        var propName = prop;
+        if (propName !== 'children') {
+            var props = properties[propName]
+            if(props['x-source'] == 'inject') {
+                inject += '\t/* '+JSON.stringify(props)+' */\n'
+                inject += '\t@Inject\n'
+                if(props['x-sourceName']) {
+                    inject += '\t@Named(value ="'+props['x-sourceName']+'")\n'
+                }
+                if(props['x-default'] === "" || props['x-default']) {
+                    inject += '\t@Default(values ="'+props['x-default']+'")\n'
+                }
+                if(props['x-form-type'] === ('collection')) {
+                    if (Object.keys(props['properties']).length > 1 || props['x-form-multifield'] === "true" || props['x-form-multifield'] === true) {
+                        inject += '\tprivate List<IComponent> '
+                    } else {
+                        inject += '\tprivate String[] '
+                    }
+                }  else {
+                    inject += '\tprivate String '
+                }
+                inject += propName
+                inject += ';'
+                inject += '\n'
+                inject += '\n'
+            } else if(props['x-form-type'] === ('reference')) {
+                inject += genNames(props.properties)
+            }
+        }
+    }
+    return inject
+}
+
+/**
+    * Used in the gen function and applies all of the getter methods. Has additional functionality to retain any custom getter methods
+    * @param {string} properties - the properties from the model JSON
+    * @param {string} customGetters - the custom getters block
+    * @return {string} the getters string block
+*/
+function genGetters(properties, customGetters) {
+    inject = ''
+    for(var prop in properties) {
+        var propName = prop;
+        var getterName = propName.charAt(0).toUpperCase()+propName.slice(1)
+        if (propName !== 'children' && (customGetters === null || !customGetters.includes('public String get' + getterName + '()'))) {
+            var props = properties[propName]
+            if(props['x-source'] == 'inject') {
+                inject += '\t/* '+JSON.stringify(props)+' */\n'
+                if(props['x-form-type'] === ('collection')) {
+                     if (Object.keys(props['properties']).length > 1 || props['x-form-multifield'] === "true" || props['x-form-multifield'] === true) {
+                        inject += '\tpublic List<IComponent> get'
+                    } else {
+                        inject += '\tpublic String[] get'
+                    }
+                } else {
+                    inject += '\tpublic String get'
+                }
+                inject += getterName + '() {\n'
+                inject += '\t\treturn '+propName+';\n'
+                inject += '\t}'
+                inject += '\n'
+                inject += '\n'
+            } else if(props['x-form-type'] === ('reference')) {
+                inject += genGetters(props.properties, customGetters)
+            }
+        }
+    }
+    return inject
+}
+
+/**
+    * Loads in the sling model template and fills in the data, inject, and getters content to make the components model
+    * @param {string} generator - the generator object
+*/
+function gen(generator) {
+
+    // load the template
+    var srcData = fs.readFileSync(generator.src).toString()
+
+    // now substitute all the values we know into the template
+    srcData = makeInitial(srcData, generator)
+
+    // if the generated file does not exist yet, create it with the template we just created
+    if(!fs.existsSync(generator.dst)) {
+        console.log('initial run for', generator.dst)
+        fs.writeFileSync(generator.dst, srcData)
+    }
+
+    // read the current file in the file system
+    var dstCurrent = fs.readFileSync(generator.dst).toString()
+
+    //retrieve the custom getters block from the current file
+    var customGetters = dstCurrent.match(/\/\/GEN\[:CUSTOMGETTERS[\s\S]*?\/\/GEN\]/g)
+    if (customGetters !== null) {
+        customGetters = customGetters[0]
+    }
+
+    // get all //GEN[: //GEN] content from the template and store it in a fragments list
+    var fragments = new Array()
+    srcData.replace(/\/\/GEN\[[\s\S]*?\/\/GEN\]/g, function(str) {
+        var name = ('name',str.match(/\/\/GEN\[:.*/)[0])
+        fragments[name] = str
+        return str
+    })
+
+    for(key in fragments) {
+        var name = key.split(':')[1]
+        var inject = ''
+
+        let data = generator.data.definitions[generator.data.modelName];
+        if('DATA' === name) {
+            inject = JSON.stringify(generator.data, true, 2)
+        } else if('INJECT' === name) {
+            inject = genNames(data.properties)
+        } else if('GETTERS' === name) {
+            inject = genGetters(data.properties, customGetters)
+        }
+
+        fragments[key] = fragments[key].replace('//GEN]', inject+'\n//GEN]')
+    }
+
+    // now replace the old fragments with the new fragments in the destination file
+    dstCurrent = dstCurrent.replace(/\/\/GEN\[[\s\S]*?\/\/GEN\]/g, function(str, offset, s) {
+        var name = ('name',str.match(/\/\/GEN\[:.*/)[0])
+        if ('CUSTOMGETTERS' === name.split(':')[1]) {
+            return customGetters
+        } else {
+            return fragments[name]
+        }
+    })
+
+    // and write the file back to the file system
+    try {
+        const original = fs.readFileSync(generator.dst).toString()
+        // only write file if not different from original
+        if(original.valueOf() !== dstCurrent.valueOf()) {
+            fs.writeFileSync(generator.dst, dstCurrent)
+        }
+    } catch(error) {
+        fs.writeFileSync(generator.dst, dstCurrent)
+    }
+}
+
+module.exports = {
+    /**
+        * Generates the sling model for the component based on the provided data
+        * @param {string} src - the source
+        * @param {string} dst - the destination
+        * @param {string} data - the data
+    */
+    generate: function(src, dst, data) {
+        gen(new Generator(src, dst, data))
+    }
+}
