03a232020c9ceb2f6660b75477fa307ac0c15114
frank_li_999 <frank.x.li@gmail.com>
2019-09-20 00:18:01 -0400

    Added angular-poc and removed authoring-poc

--- angular-ui/angular-poc/src/app/app.component.spec.ts
+++ angular-ui/angular-poc/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'authoring-poc'`, () => {
+  it(`should have as title 'angular-poc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('authoring-poc');
+    expect(app.title).toEqual('angular-poc');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('authoring-poc app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-poc app is running!');
   });
 });
