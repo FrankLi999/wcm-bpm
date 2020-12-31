0a67be4b9dee2db228b2413d9b785f41e001b6a1
FrankLi999 <frank.x.li@gmail.com>
2020-12-31 01:21:50 -0500

    Tested microk8s deployment

---
+++ ui/boot/wcm-server/src/main/java/com/bpwizard/wcm/repo/ModeshapeRepoApplication.java
@@ -0,0 +1,11 @@
+package com.bpwizard.wcm.repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ModeshapeRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModeshapeRepoApplication.class, args);
+    }
+}
